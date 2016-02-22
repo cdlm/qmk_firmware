9ab1405c2bdfb122e3c7c1a49002947119b325fe
Damien Pollet <damien.pollet@gmail.com>
2016-02-22 02:12:50 +0100

    Backlight animation: a visually linear ramp

--- keyboard/planck/keymaps/damien/keymap.c
+++ keyboard/planck/keymaps/damien/keymap.c
@@ -4,6 +4,7 @@
 #include "planck.h"
 #include "backlight.h"
 #include "action_layer.h"
+#include "timer.h"
 
 // Layer names
 #define QWY 0
@@ -89,4 +90,72 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   return MACRO_NONE;
 }
 
-// TODO check matrix_scan_user to animate lighting
+/* backlight animation experiment
+ */
+
+#define ANIM_FPS 50
+
+extern const uint16_t backlight_gamma[]; // values below
+static uint16_t animation_timer;
+void animate_backlight(uint16_t delta);
+
+void * matrix_init_user(void) {
+  animation_timer = timer_read();
+}
+
+void * matrix_scan_user(void) { // called approx. every millisecond
+  uint16_t delta = timer_elapsed(animation_timer);
+  if (delta > (1000 / ANIM_FPS)) {
+    animation_timer = timer_read();
+    animate_backlight(delta);
+  }
+  return NULL; // facepalm.
+}
+
+#define CHANNEL OCR1C
+
+void backlight_adjust(uint16_t level) {
+  // Drive LED pin low to prevent backlight blink on lowest level
+  PORTB &= ~(_BV(PORTB7));
+  CHANNEL = level;
+  // Turn PWM control off for brightness zero (and back on)
+  if (level == 0) {
+    TCCR1A &= ~(_BV(COM1C1));
+  } else {
+    TCCR1A |= _BV(COM1C1);
+  }
+}
+
+void animate_backlight(uint16_t delta) {
+  static uint16_t animate = 0;
+
+  animate += delta << 3;
+  uint8_t interp_index = animate >> 8;
+  uint8_t interp_mu = animate & 0xFF;
+
+  uint16_t brght_inf = (interp_index == 0) ? 0 : pgm_read_word(&backlight_gamma[interp_index - 1]);
+  uint16_t brght_sup = pgm_read_word(&backlight_gamma[interp_index]);
+
+  uint16_t brightness = brght_inf + ((interp_mu * (brght_sup - brght_inf)) >> 8);
+
+  backlight_adjust(brightness);
+}
+
+const uint16_t PROGMEM backlight_gamma[] = { // values generated using gamma.rb
+  28,    57,    85,    113,   142,   170,   199,   227,   255,   284,   312,   340,   369,   397,   426,   454,
+  482,   511,   539,   567,   595,   625,   655,   686,   718,   751,   785,   821,   857,   894,   933,   972,
+  1012,  1054,  1097,  1141,  1186,  1232,  1279,  1328,  1378,  1429,  1481,  1535,  1590,  1646,  1703,  1762,
+  1822,  1883,  1946,  2010,  2076,  2143,  2211,  2281,  2352,  2425,  2500,  2575,  2653,  2731,  2812,  2894,
+  2977,  3062,  3149,  3237,  3327,  3419,  3512,  3607,  3704,  3802,  3902,  4004,  4108,  4213,  4320,  4429,
+  4540,  4652,  4767,  4883,  5001,  5121,  5243,  5367,  5493,  5621,  5751,  5882,  6016,  6152,  6289,  6429,
+  6571,  6715,  6861,  7009,  7159,  7312,  7466,  7623,  7782,  7943,  8106,  8272,  8439,  8609,  8781,  8956,
+  9133,  9312,  9493,  9677,  9863,  10052, 10243, 10436, 10632, 10830, 11030, 11234, 11439, 11647, 11858, 12071,
+  12286, 12504, 12725, 12948, 13174, 13403, 13634, 13868, 14104, 14343, 14585, 14830, 15077, 15327, 15579, 15835,
+  16093, 16354, 16618, 16885, 17154, 17426, 17702, 17980, 18261, 18545, 18831, 19121, 19414, 19710, 20008, 20310,
+  20615, 20922, 21233, 21547, 21864, 22184, 22507, 22833, 23163, 23495, 23831, 24170, 24512, 24857, 25206, 25558,
+  25913, 26271, 26632, 26997, 27366, 27737, 28112, 28490, 28872, 29257, 29645, 30037, 30432, 30831, 31233, 31639,
+  32048, 32461, 32877, 33297, 33720, 34147, 34578, 35012, 35450, 35891, 36336, 36785, 37237, 37693, 38153, 38616,
+  39083, 39554, 40029, 40507, 40990, 41476, 41966, 42460, 42957, 43459, 43964, 44473, 44987, 45504, 46025, 46550,
+  47079, 47612, 48149, 48690, 49235, 49785, 50338, 50895, 51457, 52022, 52592, 53166, 53744, 54326, 54912, 55503,
+  56097, 56696, 57300, 57907, 58519, 59135, 59755, 60380, 61009, 61642, 62280, 62922, 63569, 64220, 64875, 65535
+};
